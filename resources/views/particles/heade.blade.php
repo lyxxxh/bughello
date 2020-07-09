3a02d49f6fda61f1ccab4bdb932f823437a48bb4
xxh <449399575@qq.com>
2020-07-09 21:53:15 +0800

    修改关键字

--- resources/views/particles/heade.blade.php
+++ resources/views/particles/heade.blade.php
@@ -9,8 +9,11 @@
 
 
 
-    <meta name="keywords" content="二次元资源,二次元,二次元图片,美图,图片站,死宅,hentai,动漫" />
-    <meta name="Description" content="二次元资源 多种你想要的资源" />
+    <meta name="keywords" content="二次元资源,动漫,二次元,二次元图片,动漫排行,日本动漫,新番动漫,美图,图片站,死宅,hentai">
+    <meta name="description" content="二次元资源 多种你想要的资源 动漫涩应有尽有">
+
+
+
 
     <!-- Core JavaScript Files -->
     <!--
